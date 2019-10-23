40dded90034279e9bee325e4a83fb38f65da900a
Nickg1995 <nickg1950@hotmail.com>
2019-10-23 14:08:45 +1100

    add_all

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -25,3 +25,10 @@ function handleSignUp() {
 
   });
 }
+
+function sendEmailVerification() {
+     
+      firebase.auth().currentUser.sendEmailVerification().then(function() {
+        alert('Email Verification Sent!');
+      });
+    }
